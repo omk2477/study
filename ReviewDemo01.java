d784aedbe9e91696226ba10c679787a4b62c23ba
omk2477 <omk2477@naver.com>
2021-09-17 10:16:29 +0900

    ReviewDemo01 자바 파일 while문 사용하도록 변경 (ver.5)

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -8,16 +8,26 @@ public class ReviewDemo01 {
 //        int cnt = 0;
         boolean isPrime = true;
 
-        for (int k = 2; k < number; k++) { // 반복 횟수 2회 감소
+//        for (int k = 2; k < number; k++) { // 반복 횟수 2회 감소
+//            if (number % k == 0) { // 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+//                isPrime = false;
+//                break; // 첫 번째 약수가 발견되면 for문 탈출
+//            }
+//        }
+
+        int k = 2;
+        while (k < number) {
             if (number % k == 0) { // 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
                 isPrime = false;
+                break; // 첫 번째 약수가 발견되면 for문 탈출
             }
+            k++;
         }
 
-        if (isPrime) { //카운트가 0이 유지되면 true
+        if (isPrime) {
             System.out.println(number + "은(는) 소수!");
         } else {
-            System.out.println(number + "은(는) 소수가 아닙니다!");
+            System.out.println(number + "은(는) 소수가 아닙니다");
         }
     }
 }
