2045a55227a0993ac6be339c07419f9d8670f268
omk2477 <omk2477@naver.com>
2021-09-17 10:09:18 +0900

    ReviewDemo01 자바 파일 flag 변수 사용하도록 변경 (ver.3)

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -5,15 +5,16 @@ public class ReviewDemo01 {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int cnt = 0;
+//        int cnt = 0;
+        boolean isPrime = true;
 
-        for (int k = 2; k < number; k++) {
-            if (number % k == 0) {
-                cnt++;
+        for (int k = 2; k < number; k++) { // 반복 횟수 2회 감소
+            if (number % k == 0) { // 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+                isPrime = false;
             }
         }
 
-        if (cnt == 0) {
+        if (isPrime) { //카운트가 0이 유지되면 true
             System.out.println(number + "은(는) 소수!");
         } else {
             System.out.println(number + "은(는) 소수가 아닙니다!");
